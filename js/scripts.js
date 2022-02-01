e942c0ddaee2ebf9af2acc5a7b9d694a9a040080
Niles O'Neil <nilesoneil@gmail.com>
2022-01-31 20:14:19 -0800

    more testing

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,23 @@ function wordCounter(text) {
   let wordCount = 0;
   const wordArray = text.split(" ");
   wordArray.forEach(function(word) {
+    if (!Number(element)) {
     wordCount++;
+    }
+  });
+  return wordCount;
+}
+
+function numberOfOccurrencesInText(word, text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  const wordArray = text.split(" ");
+  let wordCount = 0;
+  wordArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
+    }
   });
   return wordCount;
 }
